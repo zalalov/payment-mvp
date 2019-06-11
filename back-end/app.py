dcb909ed0b30b56dfd227bab323dd76669b79187
Ruslan Zalalov <zalalov@mail.ru>
2019-06-11 18:42:33 +0300

    Naming convention added to migrations.

--- back-end/app.py
+++ back-end/app.py
@@ -1,6 +1,19 @@
+from sqlalchemy import MetaData
 from flask import Flask, send_from_directory
+from flask_sqlalchemy import SQLAlchemy
 
 from config import get_configuration
 
+convention = {
+    "ix": 'ix_%(column_0_label)s',
+    "uq": "uq_%(table_name)s_%(column_0_name)s",
+    "ck": "ck_%(table_name)s_%(constraint_name)s",
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    "pk": "pk_%(table_name)s"
+}
+
 app = Flask(__name__)
-configuration = get_configuration()
+app.config.from_object(get_configuration())
+
+metadata = MetaData(naming_convention=convention)
+db = SQLAlchemy(app, metadata=metadata)
