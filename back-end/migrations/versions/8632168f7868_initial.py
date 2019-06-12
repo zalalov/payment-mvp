0ec922049cc342b538f207af21f71494fb9807c5
Ruslan Zalalov <zalalov@mail.ru>
2019-06-12 11:41:43 +0300

    Initial DB scheme with default values.

--- back-end/migrations/versions/8632168f7868_initial.py
+++ back-end/migrations/versions/8632168f7868_initial.py
@@ -1,18 +1,125 @@
 """initial
 
-Revision ID: f8ccd040e4bb
-Revises: 
-Create Date: 2019-06-11 18:41:01.812790
+Revision ID: 737ca72721b9
+Revises:
+Create Date: 2019-06-12 11:07:01.702359
 
 """
 from alembic import op
+from sqlalchemy.sql import table, column
 import sqlalchemy as sa
+from sqlalchemy import String, BigInteger, Numeric
+
+from config import get_configuration
+from users import generate_password_hash
+from models import Fee
 
 # revision identifiers, used by Alembic.
-revision = 'f8ccd040e4bb'
+revision = '737ca72721b9'
 down_revision = None
 branch_labels = None
 depends_on = None
+
+
+def initial_values():
+    conn = op.get_bind()
+    configuration = get_configuration()
+
+    # === Initial Currency Data ===
+    currency_tickers = ['USD', 'EUR', 'CNY']
+    currencies_table = table(
+        'currencies',
+        column('ticker', String),
+    )
+    op.bulk_insert(
+        currencies_table,
+        [{'ticker': ticker} for ticker in currency_tickers]
+    )
+    # Default hardcoded values,
+    # there should be third-party
+    # service to fetch rates
+    rates_initial = [
+        ['USD', 'EUR', 0.88],
+        ['USD', 'CNY', 6.91],
+        ['EUR', 'USD', 1.13],
+        ['EUR', 'CNY', 7.83],
+        ['CNY', 'USD', 0.14],
+        ['CNY', 'EUR', 0.12],
+    ]
+    currency_rates_table = table(
+        'currency_rates',
+        column('currency_from_id', BigInteger),
+        column('currency_to_id', BigInteger),
+        column('value', Numeric),
+    )
+    res = conn.execute("SELECT id, ticker FROM currencies")
+    currencies_fetched = res.fetchall()
+    tickers_map = {ticker: id for id, ticker in currencies_fetched}
+    op.bulk_insert(
+        currency_rates_table,
+        [
+            {
+                'currency_from_id': tickers_map[pair[0]],
+                'currency_to_id': tickers_map[pair[1]],
+                'value': pair[2]
+            } for pair in rates_initial
+        ]
+    )
+
+    # === Initial User's & Roles data ===
+    roles_table = table(
+        'roles',
+        column('name', String)
+    )
+    op.bulk_insert(
+        roles_table,
+        [
+            {'name': 'admin'},
+            {'name': 'regular'},
+        ]
+    )
+    res = conn.execute("SELECT id FROM roles WHERE name = 'admin'")
+    admin_role_id = res.fetchall()[0][0]
+    users_table = table(
+        'users',
+        column('login', String),
+        column('password', String),
+        column('role_id', BigInteger),
+    )
+    op.bulk_insert(
+        users_table,
+        [
+            {
+                'login': configuration.ADMIN_LOGIN,
+                'password': generate_password_hash(configuration.ADMIN_PASSWORD),
+                'role_id': admin_role_id
+            }
+        ]
+    )
+
+    accounts_table = table(
+        'accounts',
+        column('user_id', BigInteger),
+        column('balance', Numeric),
+        column('currency_id', BigInteger)
+    )
+    op.bulk_insert(
+        accounts_table,
+        [{'user_id': admin_role_id, 'currency_id': currency_id} for ticker, currency_id in tickers_map.items()]
+    )
+
+    fees_table = table(
+        'fees',
+        column('type', BigInteger),
+        column('percent', Numeric)
+    )
+    op.bulk_insert(
+        fees_table,
+        [
+            {'type': Fee.TYPE_INTERNAL_TRANSFER, 'percent': 0},
+            {'type': Fee.TYPE_EXTERNAL_TRANSFER, 'percent': configuration.DEFAULT_FEE},
+        ]
+    )
 
 
 def upgrade():
@@ -38,13 +145,15 @@ def upgrade():
                     sa.Column('updated_at', sa.DateTime(), server_default=sa.text('now()'), nullable=False),
                     sa.Column('id', sa.BigInteger(), nullable=False),
                     sa.Column('name', sa.Text(), nullable=False),
-                    sa.PrimaryKeyConstraint('id', name=op.f('pk_roles'))
+                    sa.PrimaryKeyConstraint('id', name=op.f('pk_roles')),
+                    sa.UniqueConstraint('name', name=op.f('uq_roles_name'))
                     )
     op.create_table('currency_rates',
                     sa.Column('created_at', sa.DateTime(), server_default=sa.text('now()'), nullable=False),
                     sa.Column('updated_at', sa.DateTime(), server_default=sa.text('now()'), nullable=False),
                     sa.Column('currency_from_id', sa.BigInteger(), nullable=False),
                     sa.Column('currency_to_id', sa.BigInteger(), nullable=False),
+                    sa.Column('value', sa.Numeric(), nullable=False),
                     sa.ForeignKeyConstraint(['currency_from_id'], ['currencies.id'],
                                             name=op.f('fk_currency_rates_currency_from_id_currencies')),
                     sa.ForeignKeyConstraint(['currency_to_id'], ['currencies.id'],
@@ -57,7 +166,6 @@ def upgrade():
                     sa.Column('id', sa.BigInteger(), nullable=False),
                     sa.Column('login', sa.Text(), nullable=False),
                     sa.Column('password', sa.Text(), nullable=False),
-                    sa.Column('salt', sa.Text(), nullable=False),
                     sa.Column('role_id', sa.BigInteger(), nullable=False),
                     sa.ForeignKeyConstraint(['role_id'], ['roles.id'], name=op.f('fk_users_role_id_roles')),
                     sa.PrimaryKeyConstraint('id', name=op.f('pk_users')),
@@ -68,8 +176,10 @@ def upgrade():
                     sa.Column('updated_at', sa.DateTime(), server_default=sa.text('now()'), nullable=False),
                     sa.Column('id', sa.BigInteger(), nullable=False),
                     sa.Column('user_id', sa.BigInteger(), nullable=False),
-                    sa.Column('balance', sa.Numeric(), nullable=False),
+                    sa.Column('balance', sa.Numeric(), server_default='0', nullable=False),
                     sa.Column('currency_id', sa.BigInteger(), nullable=False),
+                    sa.ForeignKeyConstraint(['currency_id'], ['currencies.id'],
+                                            name=op.f('fk_accounts_currency_id_currencies')),
                     sa.ForeignKeyConstraint(['user_id'], ['users.id'], name=op.f('fk_accounts_user_id_users')),
                     sa.PrimaryKeyConstraint('id', name=op.f('pk_accounts'))
                     )
@@ -81,13 +191,10 @@ def upgrade():
                     sa.Column('user_id', sa.BigInteger(), nullable=False),
                     sa.Column('account_from_id', sa.BigInteger(), nullable=False),
                     sa.Column('account_to_id', sa.BigInteger(), nullable=False),
-                    sa.Column('account_to_idd', sa.BigInteger(), nullable=False),
                     sa.ForeignKeyConstraint(['account_from_id'], ['accounts.id'],
                                             name=op.f('fk_events_account_from_id_accounts')),
                     sa.ForeignKeyConstraint(['account_to_id'], ['accounts.id'],
                                             name=op.f('fk_events_account_to_id_accounts')),
-                    sa.ForeignKeyConstraint(['account_to_idd'], ['accounts.id'],
-                                            name=op.f('fk_events_account_to_idd_accounts')),
                     sa.ForeignKeyConstraint(['user_id'], ['users.id'], name=op.f('fk_events_user_id_users')),
                     sa.PrimaryKeyConstraint('id', name=op.f('pk_events'))
                     )
@@ -100,6 +207,8 @@ def upgrade():
                     sa.ForeignKeyConstraint(['event_id'], ['events.id'], name=op.f('fk_transactions_event_id_events')),
                     sa.PrimaryKeyConstraint('id', name=op.f('pk_transactions'))
                     )
+
+    initial_values()
     # ### end Alembic commands ###
 
 
